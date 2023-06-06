4dae062c9a86a8441a0bd1f6a8161e25a03364bd
Camilo Orocú <luiscamilooc@gmail.com>
2023-06-06 15:51:35 -0600

    Ya funciona para los barcos horizontales

--- mover_barcos.py
+++ mover_barcos.py
@@ -2,7 +2,7 @@ import random
 import copy
 
 tablero_4x4 = [[2,2,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-tablero_5x5 = [[2,2,8,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0], [0,0,0,0,0]]
+tablero_5x5 = [[0,0,0,2,2],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0], [0,0,0,0,0]]
 
 def seleccionar_barcos(tablero):
     '''Esta funcion se llama primero y lo que hace es que me retorna dos tableros nuevos''' 
@@ -20,8 +20,8 @@ def mover_barcos(tablero_original):
     while i < n:
         while j < m:
             barco = tablero_original[i][j]
-            movimiento_aleatorio = random.randint(2,2) # Esta funcion dice cuantas posiciones se movera de forma aleatoria, son entre 1 y 3
-            direccion_aleatorio = random.randint(1,1) # Esta funcion dice si se movera a la derecha o izquierda, arriba o hacia abajo.  
+            movimiento_aleatorio = random.randint(1,1) # Esta funcion dice cuantas posiciones se movera de forma aleatoria, son entre 1 y 3
+            direccion_aleatorio = random.randint(2,2) # Esta funcion dice si se movera a la derecha o izquierda, arriba o hacia abajo.  
                                                           # 0 = arriba, 1 = derecha, 2 = abajo, 3 = izquierda
             # Para los barcos de 1 espacio                                     
             if tablero_original[i][j] == 1 or tablero_original[i][j] == 4:
@@ -81,20 +81,22 @@ def mover_barcos(tablero_original):
                         
                     elif direccion_aleatorio == 1: # Se movera a la derecha
                         if j + 1 + movimiento_aleatorio < n: 
-                            if tablero_nuevo[i][j + movimiento_aleatorio] == 0 and tablero_nuevo[i][j + 1 + movimiento_aleatorio] == 0  or tablero_nuevo[i][j + 1 + movimiento_aleatorio] == 0:
-                                if movimiento_aleatorio == 1:
+                            if movimiento_aleatorio == 1: # Primero hay que saber si se mueve 1 espacio o no para no borrar una parte del barco
+                                if tablero_nuevo[i][j + 1 + movimiento_aleatorio] == 0:
                                     tablero_nuevo[i][j + 1 + movimiento_aleatorio] = barco
-                                    tablero_nuevo[i][j + movimiento_aleatorio] = barco
                                     tablero_nuevo[i][j] = 0
                                     tablero_original[i][j+1] = 0
                                 else:
-                                    tablero_nuevo[i][j + 1 + movimiento_aleatorio] = barco
+                                    continue
+                            else: # Si el movimiento es de 2 o tres
+                                if tablero_nuevo[i][j + movimiento_aleatorio] == 0 and tablero_nuevo[i][j + 1 + movimiento_aleatorio] == 0: # Ver si ambos espacios estan ocupados
                                     tablero_nuevo[i][j + movimiento_aleatorio] = barco
+                                    tablero_nuevo[i][j + 1 + movimiento_aleatorio] = barco
                                     tablero_nuevo[i][j] = 0
-                                    tablero_nuevo[i][j+1] = 0
-                                    tablero_original[i][j+1] = 0
-                            else:
-                                continue
+                                    tablero_nuevo[i][j + 1] = 0 
+                                    tablero_original[i][j + 1] = 0
+                                else:
+                                    continue
                         else:
                             continue
 
@@ -112,19 +114,22 @@ def mover_barcos(tablero_original):
                             continue
                     elif direccion_aleatorio == 3: #Se mueve hacia la izquierda
                         if j - movimiento_aleatorio >= 0:
-                            if tablero_nuevo[i][j - movimiento_aleatorio] and tablero_nuevo[i][j + 1 - movimiento_aleatorio] == 0:
-                                if movimiento_aleatorio == 1:
+                            if movimiento_aleatorio == 1: # Primero hay que saber si se mueve 1 espacio o no para no borrar una parte del barco
+                                if tablero_nuevo[i][j - movimiento_aleatorio] == 0:
                                     tablero_nuevo[i][j - movimiento_aleatorio] = barco
-                                    tablero_nuevo[i][j + 1 - movimiento_aleatorio] = barco
-                                    tablero_nuevo[i][j] = 0
-                                else:
-                                    tablero_nuevo[i][j + 1 - movimiento_aleatorio] = barco
-                                    tablero_nuevo[i][j - movimiento_aleatorio] = barco
-                                    tablero_nuevo[i][j] = 0
                                     tablero_nuevo[i][j + 1] = 0
                                     tablero_original[i][j+1] = 0
-                            else:
-                                continue
+                                else:
+                                    continue
+                            else: # Si el movimiento es de 2 o tres
+                                if tablero_nuevo[i][j - movimiento_aleatorio] == 0 and tablero_nuevo[i][j + 1 - movimiento_aleatorio] == 0: # Ver si ambos espacios estan ocupados
+                                    tablero_nuevo[i][j - movimiento_aleatorio] = barco
+                                    tablero_nuevo[i][j + 1 - movimiento_aleatorio] = barco
+                                    tablero_nuevo[i][j] = 0
+                                    tablero_nuevo[i][j + 1] = 0 
+                                    tablero_original[i][j + 1] = 0
+                                else:
+                                    continue
                         else:
                             continue
 
