c872f151c1bb83db03ae6cc5e762d752199b380b
Camilo Orocú <luiscamilooc@gmail.com>
2023-06-07 17:12:13 -0600

    Qol

--- mover_barcos.py
+++ mover_barcos.py
@@ -2,7 +2,7 @@ import random
 import copy
 
 tablero_4x4 = [[2,2,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-tablero_5x5 = [[2,0,0,0,0],[2,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+tablero_5x5 = [[2,1,0,0,0],[2,0,0,1,0],[0,5,5,0,0],[0,4,0,0,0],[0,0,0,4,0]]
 
 def seleccionar_barcos(tablero):
     '''Esta funcion se llama primero y lo que hace es que me retorna dos tableros nuevos''' 
@@ -20,8 +20,8 @@ def mover_barcos(tablero_original):
     while i < n:
         while j < m:
             barco = tablero_original[i][j]
-            movimiento_aleatorio = random.randint(0,0) # Esta funcion dice cuantas posiciones se movera de forma aleatoria, son entre 1 y 3
-            direccion_aleatorio = random.randint(2,2) # Esta funcion dice si se movera a la derecha o izquierda, arriba o hacia abajo.  
+            movimiento_aleatorio = random.randint(1,3) # Esta funcion dice cuantas posiciones se movera de forma aleatoria, son entre 1 y 3
+            direccion_aleatorio = random.randint(0,3) # Esta funcion dice si se movera a la derecha o izquierda, arriba o hacia abajo.  
                                                           # 0 = arriba, 1 = derecha, 2 = abajo, 3 = izquierda
             # Para los barcos de 1 espacio                                     
             if tablero_original[i][j] == 1 or tablero_original[i][j] == 4:
@@ -64,8 +64,8 @@ def mover_barcos(tablero_original):
                         continue
 
             # Para los barcos de 2
-            if tablero_original[i][j] == 2 or tablero_original[i][j] == 5: # Para barcos que estan posicinados horizontalmente
-                if tablero_original[i][j] == barco and tablero_original[i][j + 1] == barco:
+            if tablero_original[i][j] == 2 or tablero_original[i][j] == 5:
+                if tablero_original[i][j] == barco and tablero_original[i][j + 1] == barco: # Para barcos que estan posicinados horizontalmente
                     if direccion_aleatorio == 0: # Se movera arriba
                         if i - movimiento_aleatorio >= 0:
                             if tablero_nuevo[i - movimiento_aleatorio][j] == 0 and tablero_nuevo[i - movimiento_aleatorio][j + 1] == 0:
